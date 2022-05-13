c295bdb54947294330e51e542e8a5041cf86e42c
Gabriel Gobeil <gabriel.gobeil199@gmail.com>
2022-05-12 20:29:39 -0400

    ajout des fichiers de documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,6 @@
 using Documenter, ErrorsInVariablesExtremes
 
-makedocs(sitename="ErrorsInVariablesExtremes.jl")
+makedocs(sitename="ErrorsInVariablesExtremes.jl",
+		 pages = ["index.md", "functions.md"])
+
+deploydocs(repo = "github.com/JuliaExtremes/ErrorsInVariablesExtremes.jl.git")
