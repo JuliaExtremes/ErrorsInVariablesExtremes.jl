5d4b9b846c76670c203a9d867b263a41d9b4f8fc
Gabriel Gobeil <gabriel.gobeil199@gmail.com>
2022-05-12 20:57:34 -0400

    spécifie la branche gh-pages pour la doc

--- docs/make.jl
+++ docs/make.jl
@@ -3,4 +3,7 @@ using Documenter, ErrorsInVariablesExtremes
 makedocs(sitename="ErrorsInVariablesExtremes.jl",
 		 pages = ["index.md", "functions.md"])
 
-deploydocs(repo = "github.com/JuliaExtremes/ErrorsInVariablesExtremes.jl.git")
+deploydocs(repo = "github.com/JuliaExtremes/ErrorsInVariablesExtremes.jl.git",
+		   branch = "gh-pages",
+    	   devbranch = "dev",
+    	   devurl = "dev")
