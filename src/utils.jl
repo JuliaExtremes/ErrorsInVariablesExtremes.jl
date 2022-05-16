c32a2df8ccb0c98908149823f94d142e9cb878a5
Jonathan Jalbert <jonathan.jalbert@polymtl.ca>
2022-05-15 22:14:08 -0400

    Convert directly the quantiles to a Log-normal distribution

--- src/utils.jl
+++ src/utils.jl
@@ -11,14 +11,12 @@ For each configuration of the hydrological model, the function loads the 21 quan
 """
 function load_discharge_distribution(filename::String)
 
-    
     att = ncgetatt(filename, "time", "units")
     s = split(att)
     ini = Date(s[3])
     
     t = year.(ini .+ Day.(ncread(filename, "time")))
-    
-    
+    n = length(t)
     
     id = ncread(filename, "scenario_id")[62:67,:]
     
@@ -31,44 +29,24 @@ function load_discharge_distribution(filename::String)
     end
     
     data = ncread(filename, "Dis")
-    
-    q2 = quantile(Normal(), 0.75)
-    q1 = quantile(Normal(), 0.25)
-    x2 = log.(data[16, :, :])
-    x1 = log.(data[6, :, :])
 
-    ζ = (x2 .- x1) ./ (q2 - q1)
-    η = (x1 .* q2 .- x2 .* q1) ./ (q2 - q1)
-       
+    x₁ = data[11, :, :] #Assuming this is the index of the median
+    x₂ = data[16, :, :] #Assuming this is the index of the 75th quantile
     
-    pdata = Pseudodata[]
+    # Convert the quantile to Log-normal distribution
+    μ = log.(x₁)
+    σ = (log.(x₂) .- μ)/ quantile(Normal(), .75)
+    pd = LogNormal.(μ, σ)
+    
+     pdata = Pseudodata[]
     for i in 1:S
-        push!(pdata, Pseudodata(config[i], t, LogNormal.(η[:,i], ζ[:,i])))
+        push!(pdata, Pseudodata(config[i], t, pd[:,i]))
     end
-    
+
     pensemble = Pseudoensemble("Ensemble", pdata)
     
-        
     return pensemble
 
-end
-
-"""
-    quantile2gaussian(x::Vector{<:Real}, p::Vector{<:Real})
-
-Determine the Normal distribution with `Φ(x[1]) = p[1]` and `Φ(x[2]) = p[2]`.
-"""
-function quantile2gaussian(x::Vector{<:Real}, p::Vector{<:Real})
-   
-    @assert length(x) == length(p) "The size of the quantiles vector must match the size of the probability vector."
-    @assert length(unique(p)) == 2 "Two unique quantiles are required to determine the Normal distribution."
-    
-    σ = (x[2] - x[1]) / (quantile(Normal(), p[2]) - quantile(Normal(), p[1]))
-    
-    μ = ( x[1]*quantile(Normal(),p[2]) - x[2]*quantile(Normal(),p[1]) ) / ( quantile(Normal(), p[2]) - quantile(Normal(), p[1]) )
-    
-    return Normal(μ, σ)
-    
 end
 
 """
